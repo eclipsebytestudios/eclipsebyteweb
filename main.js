31d63902c62524ea57492c643ca3e6bc1d7f6025
eclipsebytestudios <poisonacess7@gmail.com>
2026-01-09 15:38:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+function showTab(id) {
+  document.querySelectorAll('.tab').forEach(t => t.classList.add('hidden'));
+  document.getElementById(id).classList.remove('hidden');
+}
 
+function toggleSettings() {
+  document.getElementById('settings').classList.toggle('hidden');
+}
+
+function toggleNotifications() {
+  document.getElementById('notifications').classList.toggle('hidden');
+}
+
+function toggleAuth() {
+  document.getElementById('auth').classList.toggle('hidden');
+}
